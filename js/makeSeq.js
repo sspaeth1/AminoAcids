b273d9a17571ae917abce9b07ada64fa5a062d7f
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2020-04-07 18:53:41 -0500

    updated css

--- js/makeSeq.js
+++ js/makeSeq.js
@@ -1,9 +1,14 @@
 
-			var nucleotides = ["A","C","T","G"];
-			var i = 1;
-			var randNcltd = Math.round(Math.abs(getRandom(0,4)));
+var nucleotides = ["A","C","T","G"];
+var i = 1;
+var randNcltd = Math.round(Math.abs(getRandom(0,4)));
 
 
+function len()
+	{
+		var userInput = document.getElementById("len").value;
+		document.write(userInput);
+	}
 		
 function getRandom(min, max) {
  	return Math.random() * (max - min) -1 ;
@@ -12,16 +17,13 @@ function getRandom(min, max) {
 
 console.log(randNcltd);
 
-
-
- while ( i < 30){
- 	
- 	const seq = nucleotides[Math.round(Math.abs(getRandom(0,4)))];
-
-
- 	document.write(seq);
- //	document.write("<br>");
- 	i++;
- } 
-
+function makeSeq(){
+	while ( i < 30){
+		const seq = nucleotides[Math.round(Math.abs(getRandom(0,4)))];
+		document.getElementById(".seq").innerHTML = seq;
+	//	document.write("<br>");
+		i++;
+	};
+};
  
+
